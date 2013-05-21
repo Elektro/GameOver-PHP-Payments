a41c89bfebe76877f8c431d67757e7b9530aea03
Elektro <kleijn.t@gmail.com>
2013-05-21 17:49:04 +0300

    Update payments.php

--- payments.php
+++ payments.php
@@ -1,3 +1,34 @@
 <?php
 
+$shared_secret = '#_YOUR_SHARED_SECRET_#';
+
+$request = parse_signed_request($_GET['signed_request'], $shared_secret);
+
+// Parse the signed request
+function parse_signed_request($signed_request, $secret) {
+    list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+    
+    // decode the data
+    $sig = base64_url_decode($encoded_sig);
+    $data = json_decode(base64_url_decode($payload), true);
+    
+    if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+        error_log('Unknown algorithm. Expected HMAC-SHA256');
+        return null;
+    }
+    
+    // check sig
+    $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+        if ($sig !== $expected_sig) {
+        error_log('Bad Signed JSON signature!');
+        return null;
+    }
+    
+        return $data;
+    }
+    
+    function base64_url_decode($input) {
+    return base64_decode(strtr($input, '-_', '+/'));
+}
+
 ?>
